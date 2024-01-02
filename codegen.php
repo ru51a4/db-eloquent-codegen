e16337666c0b001920fb28dbf67624a4faa57c0f
ru51a4 <rruslanvaleev@yandex.ru>
2024-01-02 18:16:09 +0500

    capchta

--- codegen.php
+++ codegen.php
@@ -2,6 +2,31 @@
 /*
 Класс для генерации php фаилов бд по json схемы бд
 */
+
+//hcaptha
+
+$data = array(
+            'secret' => "",
+            'response' => $_POST['h-captcha-response']
+        );
+$verify = curl_init();
+curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+curl_setopt($verify, CURLOPT_POST, true);
+curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($verify);
+// var_dump($response);
+$responseData = json_decode($response);
+if($responseData->success) {
+    // your success code goes here
+} 
+else {
+    exit;
+}
+
+//
+
+
 $GLOBALS['id'] = uniqid();
 function force_file_put_contents($pathWithFileName, $data, $flags = 0)
 {
